4455c13f09832ffd0c26b37e704b1a6965ff1b55
Prathmesh-Tandon <151887347+Prathmesh-Tandon@users.noreply.github.com>
2025-11-05 02:13:34 +0000

    union

--- project6.c
+++ project6.c
@@ -5,12 +5,19 @@ struct pixel_t{
         unsigned char g;
         unsigned char b;
     };
+union pixel_t_1{
+        unsigned char r;
+        unsigned char g;
+        unsigned char b;
+    };
 void main(){
     int8_t g = 8;
     int size = sizeof(g);
     printf("%ld\n", sizeof(size));
     struct pixel_t p;
     printf("%ld\n", sizeof(p));
+    union pixel_t_1 f;
+    printf("%ld\n",sizeof(f));
 
 
 }
